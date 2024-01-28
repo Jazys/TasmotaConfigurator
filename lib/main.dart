db211ebc7e43d5fbd6cad61e0f5f8158744b6814
Jazys <jacquemet.julien@gmail.com>
2024-01-28 16:24:58 +0100

    add mDns client to listen esphome client. We can also configure Esphome device. Need to clean code, add some logs, refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:wifi_scan/wifi_scan.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
@@ -11,6 +13,8 @@ import 'package:simple_wifi_info/wifi_info.dart';
 import 'package:barcode_scan2/barcode_scan2.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:multicast_dns/multicast_dns.dart';
+import 'package:flutter_nsd/flutter_nsd.dart';
 
 void main() => runApp(MyApp());
 
@@ -47,6 +51,13 @@ class _WifiScannerPageState extends State<WifiScannerPage> {
   int TimeToWaitAfterGetNewIP=5;
   int TimeToWaitAfterWifiDisconnected=5;
   bool _isConfiguring = false;
+  String selectedKindOfDevice="Tasmota";
+
+
+  final flutterNsd = FlutterNsd();
+  final services = <NsdServiceInfo>[];
+  bool initialStart = true;
+  bool _scanning = false;
 
 
   @override
@@ -54,6 +65,83 @@ class _WifiScannerPageState extends State<WifiScannerPage> {
     super.initState();
     _passwordController = TextEditingController(text: '');
     _checkWifi();
+    flutterNsd.stream.listen(
+          (NsdServiceInfo service) async {
+            print('Discovered service name: ${service.name}');
+            if(service.name == tasmotaSSID && _isConfiguring) {
+              //newIpAddress=service.hostname!;
+              newIpAddress=await resolveHostnameToIPv4(service.hostname!);
+              showToast('Vous pouvez accéder à la page via ${newIpAddress} ');
+              Navigator.of(context, rootNavigator: true).pop('dialog');
+              setState(() {
+                _isConfiguring = false; // Arrêtez le chargement une fois terminé
+              });
+              stopDiscovery();
+            }
+      },
+      onError: (e) async {
+        if (e is NsdError) {
+          if (e.errorCode == NsdErrorCode.startDiscoveryFailed &&
+              initialStart) {
+            await stopDiscovery();
+          } else if (e.errorCode == NsdErrorCode.discoveryStopped &&
+              initialStart) {
+            initialStart = false;
+            await startDiscovery();
+          }
+        }
+      },
+    );
+  }
+
+  Future<String> resolveHostnameToIPv4(String hostname) async {
+    try {
+      final result = await InternetAddress.lookup(hostname);
+      final ipv4Addresses = result.where((address) => address.type == InternetAddressType.IPv4);
+
+      if (ipv4Addresses.isNotEmpty) {
+        print(ipv4Addresses.first.address);
+        return ipv4Addresses.first.address;
+      } else {
+        return 'Aucune adresse IPv4 trouvée pour $hostname.';
+      }
+    } catch (error) {
+      return 'Erreur de connexion réseau : $error';
+    }
+  }
+
+  Future<void> startDiscovery() async {
+    if (_scanning) return;
+
+    setState(() {
+      services.clear();
+      _scanning = true;
+    });
+    await flutterNsd.discoverServices('_http._tcp.');
+  }
+
+  Future<void> stopDiscovery() async {
+    if (!_scanning) return;
+
+    setState(() {
+      services.clear();
+      _scanning = false;
+    });
+    flutterNsd.stopDiscovery();
+  }
+
+  void startTimer() {
+    const timeout = Duration(seconds: 30); // Définissez la durée souhaitée
+    Timer(timeout, () {
+      setState(() {
+
+        if(_isConfiguring)
+          showToast("Recommencer l'opération ! Nom du réseau ou mot de passe incorrect");
+
+        _isConfiguring = false;
+
+      });
+    });
   }
 
   @override
@@ -102,13 +190,14 @@ class _WifiScannerPageState extends State<WifiScannerPage> {
     return ''; // Retourne une chaîne vide si aucune clé n'est trouvée
   }
 
-
-    Future<void> _checkWifi() async {
+  Future<void> _checkWifi() async {
     var wifiStatus = await Connectivity().checkConnectivity();
     if (wifiStatus == ConnectivityResult.wifi) {
       _getWifiList();
     } else {
     }
+    //discoverServices();
+    //startDiscovery();
   }
 
   Future<void> _getWifiList() async {
@@ -120,9 +209,9 @@ class _WifiScannerPageState extends State<WifiScannerPage> {
         _wifiList = wifiList.map((e) => e.ssid).toList();
         _wifiListWithoutTasmota = wifiList.map((e) => e.ssid).toList();
 
-        _wifiList = _wifiList.where((wifiName) => wifiName.contains('tasmota-')).toList();
+        _wifiList = _wifiList.where((wifiName) => wifiName.contains("${selectedKindOfDevice.toLowerCase()}-")).toList();
 
-        if(_wifiList.isNotEmpty) this.tasmotaSSID=_wifiList[0];
+        if(_wifiList.isNotEmpty) tasmotaSSID=_wifiList[0];
 
         _wifiListWithoutTasmota = _wifiListWithoutTasmota.where((wifiName) => !_wifiListWithoutTasmota.contains('tasmota-')).toList();
 
@@ -166,20 +255,36 @@ class _WifiScannerPageState extends State<WifiScannerPage> {
           },
         );
 
+        if( selectedKindOfDevice.toLowerCase() == "tasmota") {
+          await _sendGetRequest(
+              'http://192.168.4.1/wi?s1=${currentSSID}&p1=${wifiPassword}&save=');
+          showToast("Connection is processing !!!");
+          await Future.delayed(Duration(seconds: TimeToWaitAfterPassword));
+          await _sendGetRequest('http://192.168.4.1');
+          showToast("Get new IP ${newIpAddress} !!!");
+          await Future.delayed(Duration(seconds: TimeToWaitAfterGetNewIP));
+          await PluginWifiConnect.disconnect();
+          await Future.delayed(
+              Duration(seconds: TimeToWaitAfterWifiDisconnected));
+          Navigator.of(context, rootNavigator: true).pop('dialog');
+          setState(() {
+            _isConfiguring = false; // Arrêtez le chargement une fois terminé
+          });
+          showToast("Now you can access to Tasmota with configurated IP");
+        }else if(selectedKindOfDevice.toLowerCase() == "esphome"){
+          await _sendGetRequest(
+              'http://192.168.4.1/wifisave?ssid=${currentSSID}&psk=${wifiPassword}');
+          showToast("Connection is processing !!!");
+          await Future.delayed(Duration(milliseconds: 50));
+          await _sendGetRequest('http://192.168.4.1/?save');
+          await Future.delayed(Duration(seconds: 2));
+          await PluginWifiConnect.disconnect();
+          await Future.delayed(
+              Duration(seconds: TimeToWaitAfterWifiDisconnected));
+          startDiscovery();
+          startTimer();
+        }
 
-        await _sendGetRequest('http://192.168.4.1/wi?s1=${currentSSID}&p1=${wifiPassword}&save=');
-        showToast("Connection is processing !!!");
-        await Future.delayed(Duration(seconds: TimeToWaitAfterPassword));
-        await _sendGetRequest('http://192.168.4.1');
-        showToast("Get new IP ${newIpAddress} !!!");
-        await Future.delayed(Duration(seconds: TimeToWaitAfterGetNewIP));
-        await PluginWifiConnect.disconnect();
-        await Future.delayed(Duration(seconds: TimeToWaitAfterWifiDisconnected));
-        Navigator.of(context, rootNavigator: true).pop('dialog');
-        setState(() {
-          _isConfiguring = false; // Arrêtez le chargement une fois terminé
-        });
-        showToast("Now you can access to Tasmota with configurated IP");
       } catch (e) {
         // Gestion de l'erreur de connexion
       }
@@ -191,7 +296,7 @@ class _WifiScannerPageState extends State<WifiScannerPage> {
   Future<void> _sendGetRequest(url) async {
     try {
       final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
+      if (response.statusCode == 200 || response.statusCode == 302) {
         //print('Requête GET réussie');
         //print('Réponse : ${response.body}');
 
@@ -248,6 +353,22 @@ class _WifiScannerPageState extends State<WifiScannerPage> {
     return '0.0.0.0';
   }
 
+  Future<void> discoverServices() async {
+    final MDnsClient client = MDnsClient();
+    print("tototo");
+    await client.start();
+
+    // Change '_esphomelib._tcp.local' to the service type you are looking for
+    await for (PtrResourceRecord ptr in client.lookup<PtrResourceRecord>(
+        ResourceRecordQuery.serverPointer('test'))) {
+      await for (SrvResourceRecord srv in client.lookup<SrvResourceRecord>(
+          ResourceRecordQuery.service(ptr.domainName))) {
+        print('Found device at ${srv.target}:${srv.port}');
+      }
+    }
+    client.stop();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -273,10 +394,10 @@ class _WifiScannerPageState extends State<WifiScannerPage> {
               ),
             ),
             SizedBox(height: 16.0), // Ajoutez un espace vertical ici
-            const Padding(
+            Padding(
               padding: EdgeInsets.all(8.0),
               child: Text(
-                'Available Tasmota Device ',
+                'Available ${selectedKindOfDevice} Device ',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
             ),
@@ -317,7 +438,11 @@ class _WifiScannerPageState extends State<WifiScannerPage> {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   ElevatedButton(
-                    onPressed: _checkWifi,
+                    onPressed: () {_showChoicKindDeviceeDialog(context);},
+                    child: Text('0- Select Device type (default Tasmota)'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {_checkWifi(); startDiscovery();},
                     child: Text('1- Refresh available wifi device'),
                   ),
                   ElevatedButton(
@@ -326,6 +451,7 @@ class _WifiScannerPageState extends State<WifiScannerPage> {
                       setState(() {
                         currentSSID = currentSSIDInfo!.ssid!;
                       });
+                      stopDiscovery();
                     },
                     child: Text('2- Refresh your current Wifi'),
                   ),
@@ -339,7 +465,7 @@ class _WifiScannerPageState extends State<WifiScannerPage> {
                     onPressed: () {
                       _connectToWifiAndConfigure(tasmotaSSID);
                     },
-                    child: Text('4- Configure Automatically Tasmota'),
+                    child: Text('4- Configure Automatically $selectedKindOfDevice'),
                   ),
                   SizedBox(height: 8),
                   ElevatedButton(
@@ -405,5 +531,50 @@ class _WifiScannerPageState extends State<WifiScannerPage> {
       },
     );
   }
+
+  Future<void> _showChoicKindDeviceeDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: Text('Choose your device Type:', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          children: <Widget>[
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() {
+                  selectedKindOfDevice = 'Tasmota';
+                });
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Tasmota', style: TextStyle(fontSize: 18)),
+                  Icon(Icons.flash_on),
+                ],
+              ),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() {
+                  selectedKindOfDevice = 'ESPHome';
+                });
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('ESPHome', style: TextStyle(fontSize: 18)),
+                  Icon(Icons.home),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
 }
 
